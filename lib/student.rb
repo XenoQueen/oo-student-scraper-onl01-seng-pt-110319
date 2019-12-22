22cb70f51bb7bc941d165a6a1d01ddea0da78382
Maika Kailani <queenmab051@gmail.com>
2019-12-22 16:28:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-      students_array.each do |each_student|
-        self.new(each_student)
-      end
+    students_array.each do |each_student|
+      self.new(each_student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
