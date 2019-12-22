3b0954b2116498bb03b4b669a241107548c22c0e
Maika Kailani <queenmab051@gmail.com>
2019-12-22 16:28:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,12 +16,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-      @twitter = attributes_hash[:twitter]
-      @linkedin = attributes_hash[:linkedin]
-      @github = attributes_hash[:github]
-      @blog = attributes_hash[:blog]
-      @profile_quote = attributes_hash[:profile_quote]
-      @bio = attributes_hash[:bio]
+    attributes_hash.each do |k, v|
+      self.send(("#{k}="), v)
   end
 
   def self.all
